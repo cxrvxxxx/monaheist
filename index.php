d7e8e3b27d3d7fdb577d9b22d7c5f253ece669de
cxrvxxxx <brentempasisx3@gmail.com>
2023-04-29 16:15:41 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/meta.php' ?>
+    <title>Home | MonaHeist</title>
+</head>
+<body>
+    <?php include 'indludes/header.php' ?>
+</body>
+</html>
