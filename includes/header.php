0d8c6d50591623378ac174953f42b14e3713da58
Dawn <dydawnraine@gmail.com>
2023-05-01 22:24:18 +0800

    added dev and mod dot php

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <nav class="navi navbar sticky-top navbar-expand-lg bg-dark" data-bs-theme="dark">
   <div class="d-flex px-2 mx-2">
-    <a href="#" class="navbar-text navbar-brand h1">MonaHeist</a>
+    <a href="index.php" class="navbar-text navbar-brand h1">MonaHeist</a>
     <a href="#" class="navbar-text navbar-nav mx-2">Players</a>
-    <a href="#" class="navbar-text navbar-nav mx-2">Devs & Mods</a>
+    <a href="devmod.php" class="navbar-text navbar-nav mx-2">Devs & Mods</a>
     <a href="#" class="navbar-text navbar-nav mx-2">Shop</a>
   </div>
 </nav>
